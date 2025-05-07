924e9778e224c0be8702f507008813db83440c3a
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-05-07 15:41:37 -0300

    add: seção versículos com dados aleatórios do json

---
+++ public/js/versiculo.js
@@ -0,0 +1,13 @@
+fetch('../versiculos.json')
+.then(response => response.json())
+.then(data => {
+    const infoVersiculo = document.getElementById('versiculoTexto');
+    const indiceAleatorio = Math.floor(Math.random() * data.length)
+    infoVersiculo.innerHTML = `
+        <h3>${data[indiceAleatorio].versiculo}</h3>
+        <h4>${data[indiceAleatorio].localizacao}</h4>
+    `;
+})
+.catch(error => {
+    console.error('Erro ao carregar o JSON:', error);
+});
