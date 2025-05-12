04ea895f61cc0b393123752545e283970377f23b
henriquedouradoo <henrique.domingos@sptech.school>
2025-05-11 23:51:06 -0300

    add: modelagem e script do banco de dados atualizado

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,9 +6,34 @@ idUsuario int primary key auto_increment,
 nome varchar(45) not null,
 sobrenome varchar(45) not null,
 email varchar(255) not null unique,
-senha varchar(100) not null comment 'bcrypt hash',
+senha varchar(100) not null,
 dtCadastro datetime default current_timestamp
 );
 
 select * from usuario;
+
+create table versiculo (
+idVersiculo int,
+livro varchar(45) not null,
+versiculo int not null,
+interacao varchar(20) not null,
+constraint tipoInteracao check(interacao in('visualizado', 'favoritado', 'refletido')),
+dtInteracao datetime default current_timestamp,
+pkUsuario int,
+constraint pkVersiculo primary key (idVersiculo, pkUsuario),
+constraint fkVersiculoUsuario foreign key (pkUsuario) references usuario(idUsuario)
+);
+
+select * from versiculo;
+
+create table quiz (
+idQuiz int primary key auto_increment,
+pergunta int,
+pontuacao int,
+dtResposta datetime default current_timestamp,
+fkUsuario int,
+constraint fkQuizUsuario foreign key (fkUsuario) references usuario(idUsuario)
+);
+
+select * from quiz;
 
