d7a123ef40e30f7f412fd953da84109d642e90c8
henriquedouradoo <henrique.domingos@sptech.school>
2025-05-11 23:31:24 -0300

    update: ajustes script e ortografia

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -2,25 +2,13 @@ create database capitulo;
 use capitulo;
 
 create table usuario (
-idUser int primary key auto_increment,
+idUsuario int primary key auto_increment,
 nome varchar(45) not null,
 sobrenome varchar(45) not null,
-email varchar(45) not null,
-senha varchar(45) not null
+email varchar(255) not null unique,
+senha varchar(100) not null comment 'bcrypt hash',
+dtCadastro datetime default current_timestamp
 );
 
 select * from usuario;
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
 
