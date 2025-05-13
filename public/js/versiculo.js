c7da59059b328fb564ff1434b93867fed7d7ceca
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-05-13 14:45:04 -0300

    add: quiz no dashboard

--- public/js/versiculo.js
+++ public/js/versiculo.js
@@ -1,8 +1,8 @@
 fetch('../data/versiculos.json')
 .then(response => response.json())
 .then(data => {
-    const infoVersiculo = document.getElementById('versiculoTexto');
-    const indiceAleatorio = Math.floor(Math.random() * data.length)
+    var infoVersiculo = document.getElementById('versiculoTexto');
+    var indiceAleatorio = Math.floor(Math.random() * data.length)
     infoVersiculo.innerHTML = `
         <h3>${data[indiceAleatorio].versiculo}</h3>
         <h4>${data[indiceAleatorio].localizacao}</h4>
