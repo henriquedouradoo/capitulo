f37db913644391d59877579ee7dff6321e611d1d
henriquedouradoo <henrique.domingos@sptech.school>
2025-05-19 19:15:26 -0300

    add: salvando o perfil de leitor do usuário usando o quiz e plotando na dashboard

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -1,44 +1,68 @@
 var quizModel = require("../models/quizModel");
 
 function quizEnsinamentos(req, res) {
-    var idUsuario = req.body.idUsuarioServer
-    var pontuacao = req.body.pontuacaoServer
-    var idQuizEnsinamentos = req.body.idQuizEnsinamentosServer 
+  var idUsuario = req.body.idUsuarioServer;
+  var pontuacao = req.body.pontuacaoServer;
+  var idQuizEnsinamentos = req.body.idQuizEnsinamentosServer;
 
-   if (pontuacao == undefined) {
-        res.status(400).send("Você não tem pontuação válida!");
-    } else if (idUsuario == undefined) {
-        res.status(400).send("Seu id está undefined!");
-    } else if (idQuizEnsinamentos == undefined) {
-        res.status(400).send("O id do quiz está undefined!");
-    }
-
-    else {
-        quizModel.finalizarJogo(pontuacao, idUsuario, idQuizEnsinamentos)
-            .then(function (resultado) {
-                res.json(resultado);
-            })
-            .catch(function (erro) {
-                console.log(erro);
-                res.status(500).json(erro.sqlMessage);
-            });
-    }
+  if (pontuacao == undefined) {
+    res.status(400).send("Você não tem pontuação válida!");
+  } else if (idUsuario == undefined) {
+    res.status(400).send("Seu id está undefined!");
+  } else if (idQuizEnsinamentos == undefined) {
+    res.status(400).send("O id do quiz está undefined!");
+  } else {
+    quizModel
+      .finalizarJogo(pontuacao, idUsuario, idQuizEnsinamentos)
+      .then(function (resultado) {
+        res.json(resultado);
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
 }
 
 function ultimaPontuacao(req, res) {
-	var idUsuario = req.params.idUsuario;
+  var idUsuario = req.params.idUsuario;
 
-	quizModel.ultimaPontuacao(idUsuario)
-		.then((resultado) => {
-			res.status(200).json(resultado);
-		})
-		.catch((erro) => {
-			res.status(500).json(erro.sqlMessage);
-		});
+  quizModel
+    .ultimaPontuacao(idUsuario)
+    .then((resultado) => {
+      res.status(200).json(resultado);
+    })
+    .catch((erro) => {
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
+function quizHabitos(req, res) {
+  var idUsuario = req.body.idUsuarioServer;
+  var perfilLeitor = req.body.perfilLeitorServer;
+  var idQuizHabitos = req.body.idQuizHabitosServer;
+
+  if (perfilLeitor == undefined) {
+    res.status(400).send("Você não tem um perfil válido!");
+  } else if (idUsuario == undefined) {
+    res.status(400).send("Seu id está undefined!");
+  } else if (idQuizHabitos == undefined) {
+    res.status(400).send("O id do quiz está undefined!");
+  } else {
+    quizModel
+      .perfilQuiz(perfilLeitor, idUsuario, idQuizHabitos)
+      .then(function (resultado) {
+        res.json(resultado);
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+}
 
 module.exports = {
-    quizEnsinamentos,
-    ultimaPontuacao
-}
+  quizEnsinamentos,
+  ultimaPontuacao,
+  quizHabitos,
+};
--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -23,7 +23,23 @@ function ultimaPontuacao(idUsuario) {
     return database.executar(instrucao);
 }
 
+function perfilQuiz(perfilLeitor, idUsuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function quizHabitos():", perfilLeitor, idUsuario);
+
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT quizHabito (perfil, fkUsuario) VALUES
+        ('${perfilLeitor}', '${idUsuario}');
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql, [perfilLeitor, idUsuario]);
+}
+
+
 module.exports = {
     finalizarJogo,
-    ultimaPontuacao
+    ultimaPontuacao,
+    perfilQuiz
 };
