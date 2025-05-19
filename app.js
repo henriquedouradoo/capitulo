f3a2e07328546bf0e6d0f698a09a3630fc65e248
henriquedouradoo <henrique.domingos@sptech.school>
2025-05-18 22:39:09 -0300

    remove: removendo arquivos do web-data-viz não utilizados

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
 
 
 app.use(express.json());
@@ -29,8 +27,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
 
 
 app.listen(PORTA_APP, function () {
