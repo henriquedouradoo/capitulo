4e6c27e2036c379d233cbc21fe766d33b09f61d5
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-05-30 15:46:26 -0300

    add: teste de estilização dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -122,7 +122,7 @@ function dadosDashboard() {
             },
             title: {
               display: true,
-              text: "Anotações por Dia (Últimos Três Dias)",
+              text: "Anotações por Dia (Últimos Dois Dias)",
               color: "#000",
             },
           },
